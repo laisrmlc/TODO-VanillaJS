ac025a768e4691594eb04108ab31964dd7970ab4
laisrmlc <laisrmlc@gmail.com>
2024-02-29 16:06:58 +0000

    Remove innerHTML since it represents a vulnerability. Added a function to render svg icons

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
 // To prevent typos when comparing task status
 const completed = 'completed'
 const uncompleted = 'uncompleted'
-const uncheckedIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-square" viewBox="0 0 16 16">
-<path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-</svg>`
-const checkedIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
-<path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-<path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
-</svg>`
-const deleteIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/> </svg>`
+
+const checkedIconProps = {
+  paths: [
+    'M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z',
+    'M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z'
+  ],
+  classes: 'bi bi-check-square'
+}
+
+const uncheckedIconProps = {
+  paths: [
+    'M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z'
+  ],
+  classes: 'bi bi-square'
+}
+
+const deleteIconProps = {
+  paths: ['M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5'],
+  classes: 'bi bi-trash3'
+}
+
+function renderIcon(node, paths, iconClass) {
+  const iconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+
+  iconSvg.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
+  iconSvg.setAttribute('fill', 'currentColor')
+  iconSvg.setAttribute('viewBox', '0 0 16 16')
+  iconSvg.setAttribute('width', '16')
+  iconSvg.setAttribute('height', '16')
+  iconSvg.setAttribute('class', iconClass)
+
+  paths.forEach(path => {
+    const iconPath = document.createElementNS(
+      'http://www.w3.org/2000/svg',
+      'path'
+    )
+
+    iconPath.setAttribute(
+      'd',
+      path
+    )
+    iconSvg.appendChild(iconPath)
+  })
+
+  return node.appendChild(iconSvg)
+}
+
 
 // Page data
 // If it is a list, it would have a data with an array
@@ -78,7 +117,11 @@ view = {
     }
 
     // Clean list to render a new one
-    todoList.innerHTML = ''
+    while (todoList.firstChild) {
+      todoList.removeChild(todoList.firstChild)
+    }
+
+    // Clean input value
     input.value = ''
 
     tasks.forEach(task => {
@@ -91,8 +134,8 @@ view = {
       taskToRender.className = task.status === completed ? completed : ''
       
       // Button to check if the task was done
-      const checkTaskButton = document.createElement('button')    
-      checkTaskButton.innerHTML = uncheckedIcon
+      const checkTaskButton = document.createElement('button')
+      renderIcon(checkTaskButton, uncheckedIconProps.paths, uncheckedIconProps.classes)
       checkTaskButton.classList.add('button-check')
 
       // Check button event listener
@@ -103,10 +146,11 @@ view = {
       // Check which icon should be display (checked or unchecked)
       if (task.status === completed) {
         taskToRender.classList.add('completed-task')
-        checkTaskButton.innerHTML = checkedIcon
+        checkTaskButton.removeChild(checkTaskButton.firstChild)
+        renderIcon(checkTaskButton, checkedIconProps.paths, checkedIconProps.classList)
       } else {
-        taskToRender.classList.remove('completed-task')
-        checkTaskButton.innerHTML = uncheckedIcon
+        checkTaskButton.removeChild(checkTaskButton.firstChild)
+        renderIcon(checkTaskButton, uncheckedIconProps.paths, uncheckedIconProps.classes)
       }
 
       // Group of delete and check button
@@ -114,7 +158,7 @@ view = {
 
       // Delete button
       const deleteButton = document.createElement('button')
-      deleteButton.innerHTML = deleteIcon
+      renderIcon(deleteButton, deleteIconProps.paths, deleteIconProps.classes)
       deleteButton.classList.add('button-delete')
 
       // Delete button event listener
