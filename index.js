83b2a722c051c2ed5e8161524a5113b1985f4e0d
laisrmlc <laisrmlc@gmail.com>
2024-02-29 15:08:07 +0000

    Improving delegations in the project - Putting business rules inside models and outside controller

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const checkedIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="
 <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
 <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
 </svg>`
-const deleteIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-<path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-</svg>`
+const deleteIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/> </svg>`
 
 // Page data
 // If it is a list, it would have a data with an array
@@ -87,7 +85,6 @@ view = {
       
       const checkTaskButton = document.createElement('button')      
       checkTaskButton.innerHTML = uncheckedIcon
-      checkTaskButton.setAttribute('class', 'button-check')
 
       checkTaskButton.addEventListener('click', function() {
         controller.clickOnTask(task.name)
@@ -96,19 +93,15 @@ view = {
       if (task.status === completed) {
         // Set SVG markup for checked checkbox
         checkTaskButton.innerHTML = checkedIcon
-        taskToRender.classList.add('completed-task')
       } else {
         // Set SVG markup for unchecked checkbox
         checkTaskButton.innerHTML = uncheckedIcon
-        taskToRender.classList.remove('completed-task')
       }
 
       buttonWrapper.appendChild(checkTaskButton)
 
       const deleteButton = document.createElement('button')
-      deleteButton.innerHTML = deleteIcon
-      deleteButton.setAttribute('class', 'button-delete')
-
+      deleteButton.textContent = 'x'
       deleteButton.addEventListener('click', function() {
         controller.deleteTask(task.name)
       })
